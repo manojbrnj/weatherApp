ebacf085dd1713d2f88dde3942286829031e18d4
manoj barange <manojbrnj111@gmail.com>
2022-04-08 19:32:19 +0530

    weather info

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../components/style.css";
 import WeatherDetails from "./WeatherDetails";
 function SearchMain(props) {
   const [searchTerm, setSearchTerm] = useState("mumbai");
+ useEffect(()=>{
+   //function
+ },[])
   return (
-    <>
-     
+    <>     
       <div className="wrap">
         <div className="search">
           <input
@@ -15,11 +17,10 @@ function SearchMain(props) {
             placeholder="search city by name"
           />
         </div>
-        <button className="searchButton">Search</button>
+        <button className="searchButton" onClick={()=>{getWeatherInfo()}}>Search</button>
       </div>
       <WeatherDetails></WeatherDetails>
     </>
   );
 }
-
 export default SearchMain;
